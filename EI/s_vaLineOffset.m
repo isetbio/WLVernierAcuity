793f2c88a8cce8d5c09fd22e15c1494cf162574b
Brian Wandell <wandell@stanford.edu>
2016-11-26 16:00:21 -0800

    Updated and merged into the vernier acuity base script

--- EI/s_vaLineOffset.m
+++ EI/s_vaLineOffset.m
@@ -1,4 +1,4 @@
-%% s_vaLineOffset
+%% s_vaLineOffset2 - Will become LineOffset again.  Just simplifying.
 %
 %    Testing if people can see the difference between two cases:
 %      1) A straight line
@@ -16,157 +16,155 @@
 %
 % HJ/BW, ISETBIO TEAM, 2016
 
+%%
+ieInit
+
 %% Init Parameters
-% ieInit;
-if notDefined('computeStage')
-    computeStage = 'absorption';  % 'absorption', 'current'
-end
-if strcmp(computeStage, 'current')
-    error('Computing current for each time (implemented below) is wrong');
-end    
 
-%% Create display model
-ppi    = 500;          % points per inch
-imgFov = [.5 .5];      % image field of view
-sensorFov = [.15 .15]; % field of view of retina
-nFrames = 5000;        % Number of samples
+stimWeights = ieScale(fspecial('gaussian',[1,50],15),0,1);
+weights = [zeros(1, 30), stimWeights, zeros(1, 30)];
 
-if ~exist('vDist', 'var')
-vDist  = 0.3;          % viewing distance (meter)
-end
+clear sparams;
+sparams.fov = 0.5;
 
-% compute number of pixels in image
-imgSz  = round(tand(imgFov)*vDist*(ppi/dpi2mperdot(1,'m')));
+clear vparams; 
+vparams(2) = vernierP;
+vparams(2).name = 'offset'; vparams(2).bgColor = 0; vparams(2).offset = 5;
+vparams(1) = vparams(2);
+vparams(1).barWidth = 0; vparams(1).bgColor = 0.5; vparams(1).name = 'uniform';
 
-% compute actual fov of image, should be very close to desired fov
-imgFov = atand(max(imgSz)/ppi/(1/dpi2mperdot(1,'m'))/vDist);
+offset = oisCreate('vernier','add', weights,'tparams',vparams,'sparams',sparams);
+offset.visualize;
 
-% Create virtual display
-display = displayCreate('LCD-Apple', 'dpi', ppi);
-
-%% Create scenes
-% In general, we want two types of scenes: one with a constant line 
-% and one with an offset. In order to handle temporal variation, we create
-% a background scene, which is a uniform field. We use the the background
-% to blend with the two scenes and get the temporal varying OI sequence.
-
-% Init scene array
-% scene{1} - constant line
-% scene{2} - two line segments with 1 pixel offset
-% scene{3} - uniform background
-scene = cell(3, 1);
-
-% Init scene parameters
-params.display = display;
-params.sceneSz = imgSz;
-params.offset  = 0;
-params.barWidth = 1;
-params.bgColor = 0.5;
-
-% Create scene{1}: constant line
-scene{1} = sceneCreate('vernier', 'display', params);
-
-% Create scene {2}: two line segments with 1 pixel offset
-params.offset = 1;  % units: pixel
-scene{2} = sceneCreate('vernier', 'display', params);
-
-% Create scene {3}: uniform field
-params.barWidth = 0;
-scene{3} = sceneCreate('vernier', 'display', params);
-
-% set scene fov
-for ii = 1 : length(scene)
-    scene{ii} = sceneSet(scene{ii}, 'h fov', imgFov);
-    scene{ii} = sceneSet(scene{ii}, 'distance', vDist);
-end
-
-% Show radiance image (scene)
-% vcAddObject(scene{1}); vcAddObject(scene{2}); sceneWindow; 
-
-%% Compute human optical image
-% Create a typical human lens
-%   oi = oiCreate('wvf human',pupilMM,zCoefs,wave)
-oi = oiCreate('wvf human');
-
-% Compute optical image
-OIs = cell(length(scene), 1);
-for ii = 1 : length(OIs)
-    OIs{ii} = oiCompute(scene{ii}, oi);
-end
-
-% Build oiSequence
-tSamples = 50;  % 100 ms time sequence
-prependZeros = 10;
-weights = linspace(0, 1, tSamples/2 - prependZeros);
-weights = [zeros(1, prependZeros) weights ...
-           1-weights zeros(1, prependZeros)];
-
-oiSeqAligned = oiSequence(OIs{3}, OIs{1}, 0.001*(1:tSamples), weights, ...
-    'composition', 'blend');
-oiSeqOffset = oiSequence(OIs{3}, OIs{2}, 0.001*(1:tSamples), weights, ...
-    'composition', 'blend');
+vparams(2).name = 'aligned'; vparams(2).offset = 0;
+aligned = oisCreate('vernier','add', weights,'tparams',vparams,'sparams',sparams);
+aligned.visualize;
 
 %%  Compute absorptions
-nTrials = 200;
-dataAligned = [];
-dataOffset = [];
+nTrials = 5;
+tSamples = aligned.length;
 
 cMosaic = coneMosaic;
-cMosaic.setSizeToFOV(0.4 * imgFov);
+cMosaic.setSizeToFOV(0.4 * oiGet(aligned.oiFixed,'fov'));
 cMosaic.integrationTime = 0.001;
 
-emPos = zeros(tSamples, 2, nTrials);
+%% For aligned or offset
+
+tic
+
+% Eye movements and computations for each type of stimulus
+emPaths = zeros(nTrials, tSamples, 2);
 for ii = 1 : nTrials
     cMosaic.emGenSequence(tSamples);
-    emPos(:, :, ii) = cMosaic.emPositions;
+    emPaths(ii, :, :) = cMosaic.emPositions;
 end
 
-fprintf('Computing cone response: ');
-msg = '';
-for ii = 1 : tSamples
-    % compute for all trials at this time point
-    cMosaic.emPositions = squeeze(emPos(ii, :, :))';
-    if strcmp(computeStage, 'absorption')
-        % compute photon absorptions at time ii
-        cMosaic.compute(oiSeqAligned.frameAtIndex(ii), ...
-            'currentFlag', false);
-        dataAligned = cat(2, dataAligned, ...
-            reshape(cMosaic.absorptions, [], nTrials)');
-        cMosaic.compute(oiSeqOffset.frameAtIndex(ii), ...
-            'currentFlag', false);
-        dataOffset = cat(2, dataOffset, ...
-            reshape(cMosaic.absorptions, [], nTrials)');
-    elseif strcmp(computeStage, 'current')
-        % compute photo current at time ii
-        cMosaic.compute(oiSeqAligned.frameAtIndex(ii));
-        dataAligned = cat(2, dataAligned, ...
-            reshape(cMosaic.current, [], nTrials)');
-        cMosaic.compute(oiSeqOffset.frameAtIndex(ii));
-        dataOffset = cat(2, dataOffset, ...
-            reshape(cMosaic.current, [], nTrials)');
-    end     
-    
-    fprintf(repmat('\b', [1 length(msg)]));
-    msg = sprintf('%d / %d', ii, tSamples);
-    fprintf(msg);
+cMosaic.os.noiseFlag = true;
+[alignedA, alignedC] = cMosaic.compute(aligned, ...
+    'emPaths',emPaths, ...
+    'currentFlag',true);
+
+% Separate eye movements
+emPaths = zeros(nTrials, tSamples, 2);
+for ii = 1 : nTrials
+    cMosaic.emGenSequence(tSamples);
+    emPaths(ii, :, :) = cMosaic.emPositions;
 end
-fprintf('\n');
 
-%% PCA and classification
-% pca dimension reduction
+cMosaic.os.noiseFlag = true;
+[offsetA, offsetC] = cMosaic.compute(offset, ...
+    'emPaths',emPaths, ...
+    'currentFlag',true);
+toc
 
-fprintf('Dimension reduction with PCA...');
-nComponents = 20;
-coefAligned = pca(dataAligned, 'NumComponents', nComponents);
-pcaAligned = dataAligned * coefAligned;
-coefOffset = pca(dataOffset, 'NumComponents', nComponents);
-pcaOffset = dataOffset * coefOffset;
-fprintf('Done\n');
+% cMosaic.window;
 
-% svm classification
+%%  What PCA coefficient model should we use?
+
+% We replace the images by a smaller set of basis functions. Then we
+% analyze the weights of these basis functions as a function of time.
+%
+% BW should comment more.
+% outData is trial x X x Y x T
+% We want (trial*Time) x (space)
+%
+% Make a matrix with time x image (each row is an image).
+imgListAligned = zeros(nTrials*tSamples,37*37);
+for tt = 1:nTrials
+    lst = (1:tSamples) + (tSamples)*(tt-1);
+    thisTrial = squeeze(alignedA(tt,:,:,:));
+    thisTrial = permute(thisTrial,[3 1 2]);  
+    thisTrial = reshape(thisTrial,tSamples,[]);
+    imgListAligned(lst,:) = thisTrial;
+end
+
+imgListOffset = zeros(nTrials*tSamples,37*37);
+for tt = 1:nTrials
+    lst = (1:tSamples) + (tSamples)*(tt-1);
+    thisTrial = squeeze(offsetA(tt,:,:,:));
+    thisTrial = permute(thisTrial,[3 1 2]);  
+    thisTrial = reshape(thisTrial,tSamples,[]);
+    imgListOffset(lst,:) = thisTrial;
+end
+
+% Visualize
+%
+% imgList = abs(imgListOffset);  % Absorptions or current should be positive
+% mx = max(imgListOffset(:));
+% vcNewGraphWin; colormap(gray(mx));
+% for ii=1:size(imgListOffset,1)
+%     image(reshape(imgListOffset(ii,:),37,37)); 
+%     drawnow; title(sprintf('%d',ii)); 
+%     pause(0.05); 
+% end
+
+%% Home grown not-centered PCA
+
+% We make bases for each type of stimulus and then concatenate them.
+nComponents = 5;
+[U,S,V] = svd(imgListAligned,'econ');
+imageBasis = V(:,1:nComponents);
+
+[U,S,V] = svd(imgListOffset,'econ');
+imageBasis = cat(2,imageBasis,V(:,1:nComponents));
+
+vcNewGraphWin; colormap(gray(256))
+for ii=1:(2*nComponents)
+    imagesc(reshape(imageBasis(:,ii),37,37));
+    pause(0.5);
+end
+
+imgList = cat(1,imgListAligned,imgListOffset);
+weights  = imgList * imageBasis;   %
+plot(weights);
+
+%% Let's reconstruct the approximate absorption sequence
+% 
+% recon = imageBasis*weights';
+% vcNewGraphWin; colormap(gray(round(max(recon(:)))));
+% for ii=1:size(recon,2)
+%     image(reshape(recon(:,ii),37,37));
+%     title(sprintf('%d',ii));
+%     pause(0.1);
+% end
+
+
+%% svm classification
+
 fprintf('SVM Classification ');
-mdl = fitcsvm([pcaAligned; pcaOffset], ...
-    [ones(nTrials, 1); -ones(nTrials, 1)], 'KernelFunction', 'linear');
+
+% Put the weights from each trial into the rows of a matrix
+% Each row is another trial
+nWeights = size(weights,2);
+data = zeros(2*nTrials,nWeights*tSamples);
+for ii=1:(2*nTrials)
+    start = (ii-1)*tSamples + 1;
+    thisTrial = weights(start:(start+tSamples - 1),:);
+    data(ii,:) = thisTrial(:)';
+end
+
+mdl = fitcsvm(data,[ones(nTrials, 1); -ones(nTrials, 1)], 'KernelFunction', 'linear');
+
 crossMDL = crossval(mdl);
 
 func = @(y, yp, w, varargin) sum(abs(y(:, 1)-(yp(:, 1)>0)).*w);
@@ -174,3 +172,4 @@ classLoss = kfoldLoss(crossMDL, 'lossfun', func);
 fprintf('Accuracy: %.2f%% \n', (1-classLoss) * 100);
 
 
+%%
