915c844ec57d58b35d7cfdcce161e5a8e1d12a04
Brian Wandell <wandell@stanford.edu>
2016-11-29 22:00:24 -0800

    Cleaning toward looping run across many conditions and specifying offset in arc secs

--- EI/s_vaLineOffset.m
+++ EI/s_vaLineOffset.m
@@ -21,30 +21,44 @@ ieInit
 
 %% Init Parameters
 
-% Think about the time series here.
-stimWeights = ieScale(fspecial('gaussian',[1,50],15),0,1);
-weights = [zeros(1, 30), stimWeights, zeros(1, 30)];
+% Gaussian time series
+tseries = ieScale(fspecial('gaussian',[1,150],30),0,1);
 
-clear sparams;
-sparams.fov      = 0.35;
+display = displayCreate('LCD-Apple');
+display = displaySet(display,'dpi',100);  % 200 DPI
 
-clear vparams; 
-vparams(2) = vernierP;
-vparams(2).name = 'offset'; vparams(2).bgColor = 0; vparams(2).offset = 1;
-vparams(1) = vparams(2);
-vparams(1).barWidth = 0; vparams(1).bgColor = 0.5; vparams(1).name = 'uniform';
+clear sparams;  % Scene parameters in general
+sparams.fov      = 0.35;  % Deg
+sparams.distance = 0.3;   % Meters
 
-offset = oisCreate('vernier','add', weights,...
-    'tparams',vparams,'sparams',sparams);
+% Basic vernier parameters for the oiSequence
+clear vparams;
+for ii = 3:-1:1
+    vparams(ii) = vernierP;
+    vparams(ii).display = display;
+    vparams(ii).sceneSz =[150 150];
+end
+
+% Uniform field
+vparams(1).name = 'uniform'; vparams(1).bgColor = 0.5; vparams(1).barWidth = 0;
+
+% Offset Line
+vparams(2).name = 'offset';  vparams(2).bgColor = 0; vparams(2).offset = 1;
+
+% Aligned lines
+vparams(3).name = 'aligned'; vparams(3).bgColor = 0; vparams(3).offset = 0;
+
+[offset, scenes] = oisCreate('vernier','add', tseries,'tparams',vparams([1 2]),'sparams',sparams);
+offsetDeg = sceneGet(scenes{1},'degrees per sample')*vparams(2).offset;
+fprintf('Offset in arc secs %.2f\n',offsetDeg*3600);
+
 % offset.visualize;
 
-vparams(2).name = 'aligned'; vparams(2).offset = 0;
-aligned = oisCreate('vernier','add', weights,...
-    'tparams',vparams,'sparams',sparams);
+aligned = oisCreate('vernier','add', tseries,'tparams',vparams([1 3]),'sparams',sparams);
 % aligned.visualize;
 
 %%  Compute absorptions
-nTrials = 200;
+nTrials = 100;
 tSamples = aligned.length;
 
 cMosaic = coneMosaic;
@@ -79,21 +93,18 @@ offsetA = cMosaic.compute(offset, ...
     'emPaths',emPaths, ...
     'currentFlag',false);
 toc
-rows = cMosaic.rows;
-cols = cMosaic.cols;
 
 % cMosaic.window;
 
-%%  What PCA coefficient model should we use?
+%%  Reformat the time series for the PCA analysis
 
-% We replace the images by a smaller set of basis functions. Then we
-% analyze the weights of these basis functions as a function of time.
-%
-% BW should comment more.
-% outData is trial x X x Y x T
-% We want (trial*Time) x (space)
-%
-% Make a matrix with time x image (each row is an image).
+% imgListX matrix contains the temporal response for a pixel in a column
+% The rows represent time samples by number of trials
+% These are the temporal responses across all trials and time points.
+
+rows = cMosaic.rows;
+cols = cMosaic.cols;
+
 imgListAligned = zeros(nTrials*tSamples,rows*cols);
 for tt = 1:nTrials
     lst = (1:tSamples) + (tSamples)*(tt-1);
@@ -140,7 +151,7 @@ imageBasis = cat(2,imageBasis,V(:,1:nComponents));
 % end
 
 imgList = cat(1,imgListAligned,imgListOffset);
-weights  = imgList * imageBasis;   %
+tseries  = imgList * imageBasis;   %
 % plot(weights);
 
 %% Let's reconstruct the approximate absorption sequence
@@ -160,11 +171,11 @@ fprintf('SVM Classification ');
 
 % Put the weights from each trial into the rows of a matrix
 % Each row is another trial
-nWeights = size(weights,2);
+nWeights = size(tseries,2);
 data = zeros(2*nTrials,nWeights*tSamples);
 for ii=1:(2*nTrials)
     start = (ii-1)*tSamples + 1;
-    thisTrial = weights(start:(start+tSamples - 1),:);
+    thisTrial = tseries(start:(start+tSamples - 1),:);
     data(ii,:) = thisTrial(:)';
 end
 
