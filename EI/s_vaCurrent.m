866ffbe4e8da3d324dcf070cc31cdf0f46bcbe64
Brian Wandell <wandell@stanford.edu>
2016-12-26 13:33:42 -0800

    Updated the current calculation to match the properties of the absorption calculation.

--- EI/s_vaCurrent.m
+++ EI/s_vaCurrent.m
@@ -21,122 +21,210 @@
 ieInit
 
 nTrials = 100;
-tStep   = 5;   % ms
 
-% Set parameters for the vernier stimulus
-clear p; 
-p.barOffset = 0;     % Pixels on the display
-p.barWidth  = 3;     % Pixels on the display
-p.tsamples = (-60:tStep:70)*1e-3;   % In second
-p.timesd = 20*1e-3;                 % In seconds
+% Integration time and time step of the movie
+tStep   = 10;
 
-%% Create the matched vernier stimuli
+% Set the number of image bases
+nBasis = 20;
 
-[aligned, offset, scenes,tseries] = vaStimuli(p);
-% offset.visualize;
-% aligned.visualize;
-% ieAddObject(offset.oiModulated); oiWindow;
-% ieAddObject(scenes{2}); sceneWindow;
-% vcNewGraphWin; plot(p.tsamples,tseries)
+% Set basic parameters for the vernier stimulus
+clear params;
+params.barOffset = 3;     % Pixels on the display
+params.barWidth  = 2;     % Pixels on the display
+params.tsamples  = (-70:tStep:100)*1e-3;   % In second
+params.timesd  = 40*1e-3;                 % In seconds
+params.nTrials = nTrials;
+params.tStep   = tStep;
+params.nBasis  = nBasis;
 
-% Offset lines
-% offsetDeg = sceneGet(scenes{1},'degrees per sample')*vparams(2).offset;
-% fprintf('Offset in arc secs %.2f\n',offsetDeg*3600);
-
-%%  Compute absorptions for multiple trials
-
-tSamples = aligned.length;
-cMosaic = coneMosaic;
-
-% Set the mosaic size to 15 minutes (.25 deg) because that is the spatial
-% pooling size found by Westheimer and McKee
-cMosaic.setSizeToFOV(0.25);
-
-% Not sure why these have to match, but there is a bug and they do.
-cMosaic.integrationTime = aligned.timeStep;  
-
-%% For aligned or offset
-cMosaic.os.noiseFlag = 'random';
-cMosaic.noiseFlag    = 'random';
-
-emPaths = cMosaic.emGenSequence(tSamples,'nTrials',nTrials);
-
-tic
-[~, alignedC] = cMosaic.compute(aligned, ...
-    'emPaths',emPaths, ...
-    'currentFlag',true);
-toc
-% cMosaic.current = squeeze(mean(alignedC,1));
-% cMosaic.window;
-
-tic
-[~, offsetC] = cMosaic.compute(offset, ...
-    'emPaths',emPaths, ...
-    'currentFlag',true);
-toc
-% cMosaic.current = squeeze(mean(offsetC,1));
-% cMosaic.window;
-
-%%  Reformat the time series for the PCA analysis
-
-% imgListXXX matrix contains the temporal response for a pixel in a column
-% The rows represent time samples by number of trials. These are the
-% temporal responses across all trials and time points.
-
-imgListAligned = trial2Matrix(alignedC,cMosaic);
-imgListOffset  = trial2Matrix(offsetC,cMosaic);
-
-% PCA basis only for training data
-% train with 80% of data
-label = [ones(nTrials, 1); -ones(nTrials, 1)];
-train_index = zeros(nTrials, 1);
-train_index(randperm(nTrials, 0.8*nTrials)) = 1;
-train_index = train_index > 0;
-imgListAlignedTrain = trial2Matrix(alignedC(train_index, :,:,:), cMosaic);
-imgListOffsetTrain = trial2Matrix(offsetC(train_index, :,:,:), cMosaic);
-
-%% Not-centered PCA (no demeaning, so first PC is basically the mean)
-
-% We make bases for each type of stimulus and then concatenate them.
-nComponents = 4;
-[~,~,V] = svd(imgListAlignedTrain,'econ');
-imageBasis = V(:,1:nComponents);
-
-[~,~,V] = svd(imgListOffsetTrain,'econ');
-imageBasis = cat(2,imageBasis,V(:,1:nComponents));
-
-imgList = cat(1,imgListAligned,imgListOffset);
-% imgList = cat(1,imgListAligned,imgListAligned);
-
-% Time series of weights
-weightSeries  = imgList * imageBasis;
-
-
-%% svm classification
-
-fprintf('SVM Classification ');
-
-% Put the weights from each trial into the rows of a matrix
-% Each row is another trial
-nWeights = size(weightSeries,2);
-data = zeros(2*nTrials,nWeights*tSamples);
-for ii=1:(2*nTrials)
-    start = (ii-1)*tSamples + 1;
-    thisTrial = weightSeries(start:(start+tSamples - 1),:);
-    data(ii,:) = thisTrial(:)';
+% If already computed, use the imageBasis.  If not, make an image basis.
+tmp = [];
+if exist('imageBasisCurrent.mat','file'), tmp = load('imageBasisCurrent'); end
+if isfield(tmp,'basisParameters')
+    basisParameters = tmp.basisParameters;
+    if isequal(basisParameters.barWidth,params.barWidth) && ...
+            isequal(basisParameters.timesd, params.timesd) && ...
+            isequal(basisParameters.tStep,params.tStep) && ...
+            basisParameters.nBasis >= params.nBasis
+        disp('Loading image basis because parameters match')
+        load('imageBasisAbsorptions','imageBasis');
+    else
+        disp('Creating new image basis - parameters do not match')
+        imageBasis = vaCurrentPCA(params);
+    end
+else
+    disp('Creating new image basis - can not find parameters in file')
+    imageBasis = vaCurrentPCA(params);
 end
 
-%
+% % % Have a look if you like
+% vcNewGraphWin; colormap(gray(256))
+% mx = max(imageBasis(:)); mn = min(imageBasis(:));
+% for ii=1:params.nBasis
+%     imagesc(reshape(imageBasis(:,ii),cMosaic.rows,cMosaic.cols),[mn mx]);
+%     title(sprintf('Basis %d',ii));
+%     pause(0.5);
+% end
+
+% We print these out at the end
+X = zeros(1,7);
+P = zeros(1,7);
+barOffset = 0:1:6;
+
+%% Create the aligned and offset vernier stimuli
+
+for bb = 1:numel(barOffset)
+    
+    params.barOffset = barOffset(bb);
+    [aligned, offset, scenes,tseries] = vaStimuli(params);
+    % offset.visualize;
+    % aligned.visualize;
+    % ieAddObject(offset.oiModulated); oiWindow;
+    % ieAddObject(scenes{2}); sceneWindow;
+    % vcNewGraphWin; plot(params.tsamples,tseries)
+    
+    % Offset lines
+    % offsetDeg = sceneGet(scenes{1},'degrees per sample')*vparams(2).offset;
+    % fprintf('Offset in arc secs %.2f\n',offsetDeg*3600);
+    
+    %%  Compute absorptions for multiple trials
+    
+    tSamples = aligned.length;
+    
+    cMosaic = coneMosaic;
+    
+    % Set the mosaic size to 15 minutes (.25 deg) because that is the spatial
+    % pooling size found by Westheimer and McKee
+    cMosaic.setSizeToFOV(0.25);
+    
+    % Not sure why these have to match, but there is a bug if they don't.
+    cMosaic.integrationTime = aligned.timeStep;
+    
+    %% For aligned or offset
+    
+    cMosaic.noiseFlag    = 'random';
+    
+    emPaths  = cMosaic.emGenSequence(tSamples,'nTrials',nTrials);
+    
+    tic
+    [~, alignedC] = cMosaic.compute(aligned,'currentFlag',true,'emPaths',emPaths);
+    toc
+    
+    tic
+    [~, offsetC] = cMosaic.compute(offset,'currentFlag',true,'emPaths',emPaths);
+    toc
+    
+    % cMosaic.window;
+    
+    %%  Reformat the time series for the PCA analysis
+    
+    % imgListX matrix contains the temporal response for a pixel in a column
+    % The rows represent time samples by number of trials
+    % These are the temporal responses across all trials and time points.
+    
+    imgListAligned = trial2Matrix(alignedC,cMosaic);
+    imgListOffset  = trial2Matrix(offsetC,cMosaic);
+    
+    % Visualize the sequence
+    %
+    % imgList = abs(imgListOffset);  % Absorptions or current should be positive
+    % mx = max(imgListOffset(:));
+    % vcNewGraphWin; colormap(gray(mx));
+    % for ii=1:size(imgListOffset,1)
+    %     image(reshape(imgListOffset(ii,:),rows*cols));
+    %     drawnow; title(sprintf('%d',ii));
+    %     pause(0.05);
+    % end
+    
+    %% Not-centered PCA (no demeaning, so first PC is basically the mean)
+    
+    % Could shrink nBasis here ... or not
+    % % imageBasis = imageBasis(:,1:nBasis);
+    
+    imgList = cat(1,imgListAligned,imgListOffset);
+    % imgList = cat(1,imgListAligned,imgListAligned);
+    
+    % Time series of weights
+    weightSeries  = imgList * imageBasis;
+    
+    %% Let's see if we can reduce the dimensionality of the time series
+    %
+    % The reasons is that the photocurrent time series, which smooths the
+    % signal over time, performs much better with the SVM.  So, I think that
+    % smoothing the time series in the absorptions would allow the SVM to find
+    % a good solution here, as well.
+    %
+    % Now the weight series for each image basis comprises 150 numbers for each
+    % of 600 trials. We frame this as 150 x 600 and reduce it to [150 x
+    % nTimeBasis]*wgts
+    
+    % These are the time series for each of the trials
+    % foo = reshape(weightSeries(:,1),tSamples,2*nTrials);
+    % vcNewGraphWin; plot(foo);
+    % [U,S,T] = svd(foo,'econ');
+    % vcNewGraphWin;
+    % plot(U(:,1));
+    %
+    % wgt = U(:,1:3)'*foo;
+    % vcNewGraphWin;
+    % plot3(wgt(1,1:300),wgt(2,1:300),wgt(3,1:300),'ro')
+    % hold on;
+    % plot3(wgt(1,301:600),wgt(2,301:600),wgt(3,301:600),'go')
+    % hold off
+    
+    %% Start classification training
+    
+    fprintf('SVM Classification ');
+    
+    % Put the weights from each trial into the rows of a matrix
+    % Each row is another trial
+    nWeights = size(weightSeries,2);
+    data = zeros(2*nTrials,nWeights*tSamples);
+    for ii=1:(2*nTrials)
+        start = (ii-1)*tSamples + 1;
+        thisTrial = weightSeries(start:(start+tSamples - 1),:);
+        data(ii,:) = thisTrial(:)';
+    end
+    label = [ones(nTrials, 1); -ones(nTrials, 1)];
+    
+    % func = @(y, yp, w, varargin) sum(abs(y(:, 1)-(yp(:, 1)>0)).*w);
+    % classLoss = kfoldLoss(crossMDL, 'lossfun', func);
+    
+    % Select some of the data (80%) as the training set.
+    train_index = zeros(nTrials, 1);
+    train_index(randperm(nTrials, round(0.8*nTrials))) = 1;
+    train_index = train_index > 0;
+    
+    % The aligned and offset trials are still matched
+    train_index = [train_index; train_index];
+    
+    % Fit the SVM model.
+    mdl = fitcsvm(data(train_index, :), label(train_index), ...
+        'KernelFunction', 'linear');
+    
+    % predict the data not in the training set.
+    yp = predict(mdl, data(~train_index, :));
+    classLoss = sum(label(~train_index) ~= yp) / length(yp);
+    X(bb) = barOffset(bb); P(bb) = (1-classLoss) * 100;
+    fprintf('Accuracy for held out data: (%d, %.2f%%) \n', barOffset(bb), (1-classLoss) * 100);
+end
+
+%% Dump out for saving and plotting
+
+params
+s = sprintf('X = ['); s = [s, sprintf('%d ',X)]; s = [s , sprintf(']')];
+disp(s)
+s = sprintf('P = ['); s = [s, sprintf('%.2f ',P)]; s = [s , sprintf(']')];
+disp(s)
+
+%% Run cross validation
+
+% I don't understand this yet, so I am skipping (BW)
+
+% crossMDL = crossval(mdl);    % I don't understand this syntax
 % func = @(y, yp, w, varargin) sum(abs(y(:, 1)-(yp(:, 1)>0)).*w);
 % classLoss = kfoldLoss(crossMDL, 'lossfun', func);
-train_index = [train_index; train_index];
-mdl = fitcsvm(data(train_index, :), label(train_index), ...
-    'KernelFunction', 'linear');
-
-% predict on test set
-yp = predict(mdl, data(~train_index, :));
-classLoss = sum(label(~train_index) ~= yp) / length(yp);
-fprintf('Accuracy: %.2f%% \n', (1-classLoss) * 100);
 
 %% Visualize the classification function
 
@@ -151,30 +239,32 @@ fprintf('Accuracy: %.2f%% \n', (1-classLoss) * 100);
 %
 %  x'*beta + Bias, I think.
 %
-%  When we get mdl.Beta, it has size of (2*nComponents)*tSamples.   So, if
+%  When we get mdl.Beta, it has size of tSamples*nBasis.   So, if
 %  there are 150 tSamples and 10 spatial image components, then Beta is
 %  1500.
 %
-%  I think that on a single trial we have 150*10 numbers, which we derive
-%  by taking the inner product of the 10 spatial basis functions on every
-%  temporal sample.  If we want to express the 1500 numbers as a time
-%  series, we would multiple the 10 weights times the spatial image basis
-%  at each of the 150 time points.  So the classifier is a movie.
+%  I think that on a single trial we have tSamples*nBasis numbers.  If we
+%  want to express these  numbers as a movie, we multiply the weights times
+%  the spatial image basis at each of the time points. So the classifier is
+%  a movie.
 %
-beta = mdl.Beta;
-nBasis = size(imageBasis,2);
-img = zeros(cMosaic.rows,cMosaic.cols,tSamples);
-
-colormap('default')
-for ii=1:tSamples
-    lst = (1:nBasis) + (ii-1)*nBasis;
-    % lst = ii:tSamples:length(beta);
-    tmp = imageBasis*beta(lst);
-    img(:,:,ii) = reshape(tmp,cMosaic.rows,cMosaic.cols);
-    imagesc(img(:,:,ii),[-.5 .5]); title(ii); colorbar; pause(.2);
-end
-
-vcNewGraphWin; 
-colormap('default'); ieMovie(img);
+%
+%
+% % These are the beta weights we multiply
+% beta = mdl.Beta;
+% % nBasis = size(imageBasis,2);
+% img = zeros(cMosaic.rows,cMosaic.cols,tSamples);
+%
+% colormap('default')
+% for ii=1:tSamples
+%     lst = (1:nBasis) + (ii-1)*nBasis;
+%     % lst = ii:tSamples:length(beta);
+%     tmp = imageBasis*beta(lst);
+%     img(:,:,ii) = reshape(tmp,cMosaic.rows,cMosaic.cols);
+%     imagesc(img(:,:,ii),[-.5 .5]); title(ii); colorbar; pause(.2);
+% end
+%
+% vcNewGraphWin;
+% colormap('default'); ieMovie(img);
 
 %%
