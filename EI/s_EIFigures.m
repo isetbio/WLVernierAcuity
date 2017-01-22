06445162692dfca5213a40be26f6d782a001bdac
Brian Wandell <wandell@stanford.edu>
2017-01-22 13:59:09 -0800

    Neatening for presentation

--- EI/s_EIFigures.m
+++ EI/s_EIFigures.m
@@ -17,6 +17,47 @@ dfiles = dir(fullfile(ddir,'spatialBarLength*'));
 chdir(ddir);
 d = load(dfiles(end).name);
 
+%%  Peak Luminance
+dfiles = dir('example*');
+
+nFiles = length(dfiles);
+peakLum = zeros(1,nFiles);
+PCall = zeros(numel(barOffset),nFiles);
+for ii=1:nFiles
+    load(dfiles(ii).name);
+    display = params.vernier.display;
+    peakLum(ii) = displayGet(display,'peak luminance');
+    PCall(:,ii) = PC(:);
+end
+
+%% 
+mesh(PCall)
+
+%%
+[p,idx] = sort(log10(peakLum));
+PCsorted = PCall(:,idx);
+
+p(3) = p(3) + 0.01;
+barOffsetSec = barOffset*sceneGet(scenes{2},'degrees per sample')*3600;
+mesh(p,barOffsetSec,PCsorted)
+
+% Interpolate the data
+[X,Y] = meshgrid(p(1):0.2:p(end),barOffsetSec(1):2:barOffsetSec(end));
+PCInterp = interp2(p,barOffsetSec,PCsorted,X,Y); 
+surf(X,Y,PCInterp)
+xlabel('Max luminance (log 10 cd/m^2)');
+ylabel('Offset (arc sec)')
+zlabel('Percent correct')
+% view(-23.5, 22);   % Reasonable view
+% view(90,-90);     % Good view for curve
+
+%%
+
+vcNewGraphWin;
+mesh(p,barOffsetSec,PCsorted);
+xlabel('Peak luminance'); ylabel('Bar offset'); zlabel('Percent correct');
+
+
 %%
 
 % Either this
--- functions/vaImageBasis.m
+++ functions/vaImageBasis.m
@@ -1,19 +1,25 @@
-function vaImageBasis(params)
+function data = vaImageBasis(params)
+% VAIMAGEBASIS - Compute image basis for vernier acuity task
 %
 %
-%
-%
+% Probably should just return the data and plot with ieMovie on the return
 
+%%
 data = vaPCA(params);
 rows = sqrt(size(data,1));
 cols = rows;
-vcNewGraphWin; 
-colormap(gray(256))
-mx = max(data(:)); mn = min(data(:));
-for ii=1:params.nBasis
-    imagesc(reshape(data(:,ii),rows,cols),[mn mx]);
-    title(sprintf('Basis %d',ii));
-    pause(0.5);
-end
+data = reshape(data,rows,cols,[]);
+
+%%  Maybe this should be an ieMovie call?
+ieMovie(data,'FrameRate',4);
+
+% vcNewGraphWin; 
+% colormap(gray(256))
+% mx = max(data(:)); mn = min(data(:));
+% for ii=1:params.nBasis
+%     imagesc(reshape(data(:,ii),rows,cols),[mn mx]);
+%     title(sprintf('Basis %d',ii));
+%     pause(0.5);
+% end
 
 end
